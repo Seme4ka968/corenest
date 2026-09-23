1816463feab694448646f3b71ceec2a4ae02ae4f
Seme4ka968 <143346151+Seme4ka968@users.noreply.github.com>
2026-09-23 23:35:48 +0300

    Create audio.h

---
+++ src/audio.h
@@ -0,0 +1,13 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+bool audio_init(double sample_rate);
+void audio_deinit(void);
+void audio_push_sample(int16_t left, int16_t right);
+size_t audio_push_batch(const int16_t *data, size_t frames);
+
+#endif
