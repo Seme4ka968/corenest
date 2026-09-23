4855fb0834184f2b2606a40c18e2013e0fe695bf
Seme4ka968 <143346151+Seme4ka968@users.noreply.github.com>
2026-09-23 23:35:14 +0300

    Create video.h

---
+++ src/video.h
@@ -0,0 +1,14 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "libretro.h"
+
+bool video_init(unsigned width, unsigned height);
+void video_deinit(void);
+void video_refresh(const void *data, unsigned width, unsigned height,
+                   size_t pitch, enum retro_pixel_format fmt);
+void video_present(void);
+
+#endif
