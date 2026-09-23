f1922ba89ef7d93bb2fd9e4b20a1655fbd789806
Seme4ka968 <143346151+Seme4ka968@users.noreply.github.com>
2026-09-23 23:36:16 +0300

    Create input.h

---
+++ src/input.h
@@ -0,0 +1,14 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "libretro.h"
+
+bool input_init(void);
+void input_deinit(void);
+void input_poll(void);
+int16_t input_state(unsigned port, unsigned device,
+                    unsigned index, unsigned id);
+
+#endif
