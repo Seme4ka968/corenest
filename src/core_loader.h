b8e3137f63f1e414228a5d18fc8eb195cd86b38d
Seme4ka968 <143346151+Seme4ka968@users.noreply.github.com>
2026-09-23 23:34:07 +0300

    Create core_loader.h

---
+++ src/core_loader.h
@@ -0,0 +1,33 @@
+#ifndef CORE_LOADER_H
+#define CORE_LOADER_H
+
+#include <stdbool.h>
+#include "libretro.h"
+
+typedef struct {
+    void *handle;
+    void (*retro_init)(void);
+    void (*retro_deinit)(void);
+    unsigned (*retro_api_version)(void);
+    void (*retro_get_system_info)(struct retro_system_info *info);
+    void (*retro_get_system_av_info)(struct retro_system_av_info *info);
+    void (*retro_set_environment)(retro_environment_t);
+    void (*retro_set_video_refresh)(retro_video_refresh_t);
+    void (*retro_set_audio_sample)(retro_audio_sample_t);
+    void (*retro_set_audio_sample_batch)(retro_audio_sample_batch_t);
+    void (*retro_set_input_poll)(retro_input_poll_t);
+    void (*retro_set_input_state)(retro_input_state_t);
+    void (*retro_set_controller_port_device)(unsigned port, unsigned device);
+    bool (*retro_load_game)(const struct retro_game_info *game);
+    void (*retro_unload_game)(void);
+    void (*retro_run)(void);
+    void (*retro_reset)(void);
+    size_t (*retro_serialize_size)(void);
+    bool (*retro_serialize)(void *data, size_t size);
+    bool (*retro_unserialize)(const void *data, size_t size);
+} core_api_t;
+
+bool core_load(const char *path, core_api_t *api);
+void core_unload(core_api_t *api);
+
+#endif
