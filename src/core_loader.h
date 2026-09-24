2b054311a4c2218d5f50e522180d1b0ce1fda576
Seme4ka968 <143346151+Seme4ka968@users.noreply.github.com>
2026-09-24 09:04:29 +0300

    Update core_loader.h

--- src/core_loader.h
+++ src/core_loader.h
@@ -26,6 +26,8 @@ typedef struct {
     size_t (*retro_serialize_size)(void);
     bool (*retro_serialize)(void *data, size_t size);
     bool (*retro_unserialize)(const void *data, size_t size);
+    void *(*retro_get_memory_data)(unsigned id);
+    size_t (*retro_get_memory_size)(unsigned id);
 } core_api_t;
 
 bool core_load(const char *path, core_api_t *api);
