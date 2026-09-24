82f74207db5b7b81b446d761e5bb2c20cf52a2a6
Seme4ka968 <143346151+Seme4ka968@users.noreply.github.com>
2026-09-24 08:35:34 +0300

    Update video.h

--- src/video.h
+++ src/video.h
@@ -12,6 +12,7 @@ void video_refresh(const void *data, unsigned width, unsigned height,
 void video_present(void);
 
 void video_toggle_fullscreen(void);
+void video_set_position(int x, int y);
 void video_set_title(const char *title);
 void video_set_scale(int scale);
 int  video_get_scale(void);
