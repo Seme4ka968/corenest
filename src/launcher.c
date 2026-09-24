b9bc505dd84a613f9c48211120e779475f499125
Seme4ka968 <143346151+Seme4ka968@users.noreply.github.com>
2026-09-24 22:05:39 +0300

    refactor: use organized subsystem headers

--- src/launcher.c
+++ src/launcher.c
@@ -10,11 +10,11 @@
 
 #include "libretro.h"
 #include "core/core.h"
-#include "environment.h"
-#include "video.h"
-#include "audio.h"
-#include "input.h"
-#include "save.h"
+#include "core/environment.h"
+#include "drivers/video/video.h"
+#include "drivers/audio/audio.h"
+#include "drivers/input/input.h"
+#include "save/save.h"
 
 #ifdef _WIN32
   #include <direct.h>
