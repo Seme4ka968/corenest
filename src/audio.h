4772810b2a9e46b7cf252ecfee9cc8748dca7ee3
Seme4ka968 <143346151+Seme4ka968@users.noreply.github.com>
2026-09-24 09:26:56 +0300

    Update audio.h

--- src/audio.h
+++ src/audio.h
@@ -8,6 +8,7 @@
 bool audio_init(double sample_rate);
 void audio_deinit(void);
 void audio_push_sample(int16_t left, int16_t right);
+void audio_set_mute(int mute);
 size_t audio_push_batch(const int16_t *data, size_t frames);
 
 #endif
