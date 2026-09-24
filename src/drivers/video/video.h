68c1df640530df3125348f266f6f75bc52a72d55
Seme4ka968 <143346151+Seme4ka968@users.noreply.github.com>
2026-09-24 22:04:59 +0300

    refactor: organize subsystem source tree

---
+++ src/drivers/video/video.h
@@ -0,0 +1,26 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include "libretro.h"
+
+bool video_init(unsigned width, unsigned height);
+void video_deinit(void);
+void video_refresh(const void *data, unsigned width, unsigned height,
+                   size_t pitch, enum retro_pixel_format fmt);
+void video_present(void);
+
+void video_toggle_fullscreen(void);
+void video_set_position(int x, int y);
+void video_set_title(const char *title);
+void video_set_scale(int scale);
+int  video_get_scale(void);
+void video_toggle_filter(void);
+int  video_is_fullscreen(void);
+
+void video_set_fast_forward(int on);
+int  video_is_fast_forward(void);
+int  video_screenshot(const char *path);
+
+#endif
